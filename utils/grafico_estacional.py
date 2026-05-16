1e1490f7e5ebbdd87c7ba750c2877bcafe118007
JairRamosG <ramosgonzalezjair@gmail.com>
2026-05-15 18:12:45 -0600

    ajuste función gráfico estacional

--- utils/grafico_estacional.py
+++ utils/grafico_estacional.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import STL
 
-def grafico_estacional(df, col, periodo, seasonal=True):
+def grafico_estacional(df, col, periodo, estacionalidad = 'meses', seasonal=True):
     df_c = df.copy()
     
     # Crear columnas de tiempo
@@ -16,13 +16,21 @@ def grafico_estacional(df, col, periodo, seasonal=True):
         df_c[col + '_seas'] = stl.seasonal
         col = col + '_seas'
     
-    # Agrupar por mes y hora para obtener un valor único por combinación
-    df_agg = df_c.groupby(['mes', 'hora'])[col].mean().reset_index()
+    if estacionalidad == 'meses':
+        # Agrupar por mes y hora para obtener un valor único por combinación
+        df_agg = df_c.groupby(['mes', 'hora'])[col].mean().reset_index()
+        
+        # Pivotear: filas = hora, columnas = mes, valores = col (promedio)
+        df_pivot = df_agg.pivot(index='hora', columns='mes', values=col)
     
-    # Pivotear: filas = hora, columnas = mes, valores = col (promedio)
-    df_pivot = df_agg.pivot(index='hora', columns='mes', values=col)
-    
-    # Graficar
+    if estacionalidad == 'años':
+        # Agrupar por año y hora para obtener un valor único por combinación
+        df_agg = df_c.groupby(['año', 'hora'])[col].mean().reset_index()
+        
+        # Pivotear: filas = hora, columnas = año, valores = col (promedio)
+        df_pivot = df_agg.pivot(index='hora', columns='año', values=col)
+
+        # Graficar
     ax = df_pivot.plot(figsize=(18, 6))
     plt.title(f'Componente estacional - {col}')  
     plt.xlabel('Hora')
